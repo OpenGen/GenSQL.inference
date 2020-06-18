329496932a78ee7044dcc9928a445b87376121bb
Nick Charchut <nicholas.charchut@gmail.com>
2020-06-18 14:06:01 -0400

    Categorical pGPM (#34)
    * Category pGPM implementation and testing

---
+++ src/inferenceql/inference/gpm/primitive_gpms/categorical.clj
@@ -0,0 +1,81 @@
+(ns inferenceql.inference.gpm.primitive-gpms.categorical
+  (:require [inferenceql.inference.gpm.proto :as gpm.proto]
+            [inferenceql.inference.utils :as utils]
+            [inferenceql.inference.primitives :as primitives]))
+
+(defrecord Categorical [var-name parameters suff-stats hyperparameters]
+  gpm.proto/GPM
+  (logpdf [this targets constraints]
+    (let [x (get targets var-name)
+          x' (get constraints var-name)
+          constrained? (not (nil? x'))]
+      (cond
+        (nil? x) 0
+        constrained? (if (= x x') 0 ##-Inf)
+        :else (let [counts (:counts suff-stats)
+                    alpha  (:alpha hyperparameters)
+                    numer  (Math/log (+ alpha (get counts x)))
+                    denom  (Math/log (+ (* alpha (count counts))
+                                        (reduce + (vals counts))))]
+                (- numer denom)))))
+  (simulate [this targets constraints n-samples]
+    (let [p (->> (keys (:p parameters))
+                 (reduce (fn [m k]
+                           (assoc m k (gpm.proto/logpdf this {var-name k} {})))
+                         {})
+                 (assoc {} :p))]
+    (primitives/simulate n-samples :log-categorical p)))
+
+  gpm.proto/Incorporate
+  (incorporate [this values]
+    (let [x (get values var-name)]
+      (-> this
+          (assoc :suff-stats (-> suff-stats
+                                 (update :n inc)
+                                 (update-in [:counts x] (fnil inc 0)))))))
+  (unincorporate [this values]
+    (let [x (get values var-name)]
+      (-> this
+          (assoc :suff-stats (-> suff-stats
+                                 (update :n dec)
+                                 (update-in [:counts x] dec))))))
+
+  gpm.proto/Score
+  (logpdf-score [this]
+    (let [counts (:counts suff-stats)
+          n (:n suff-stats)
+          k (count counts)
+          alpha (:alpha hyperparameters)
+          a (* k alpha)
+          lg (reduce +
+               (map (fn [v] (primitives/gammaln (+ v alpha)))
+                    (vals counts)))]
+      (+ (primitives/gammaln a)
+         (- (primitives/gammaln (+ a n)))
+         lg
+         (* -1 k (primitives/gammaln alpha))))))
+
+(defn categorical?
+  "Checks if the given pGPM is Categorical."
+  [stattype]
+  (and (record? stattype)
+       (instance? Categorical stattype)))
+
+(defn hyper-grid
+  "Hyperparameter grid for the Categorical variable, used in column hyperparameter inference
+  for Column GPMs."
+  [data n-grid]
+  (let [grid (utils/log-linspace 1 (count data) n-grid)]
+    {:alpha grid}))
+
+(defn spec->categorical
+  "Casts a CrossCat category spec to a Categorical variable."
+  ([var-name parameters]
+   (let [probs   (:p parameters)
+         options (keys probs)
+         counts  (repeat (count options) 0)]
+     (spec->categorical var-name parameters {:n 0 :counts (zipmap options counts)} {:alpha 1})))
+  ([var-name parameters suff-stats]
+   (spec->categorical var-name parameters suff-stats {:alpha 1}))
+  ([var-name parameters suff-stats hyperparameters]
+   (->Categorical var-name parameters suff-stats hyperparameters)))
---
+++ test/inferenceql/inference/gpm/primitive_gpms/categorical_test.clj
@@ -0,0 +1,77 @@
+(ns inferenceql.inference.gpm.primitive-gpms.categorical-test
+  (:require [clojure.test :as test :refer [deftest is]]
+            [inferenceql.inference.gpm.proto :as gpm.proto]
+            [inferenceql.inference.utils :as utils]
+            [inferenceql.inference.gpm.primitive-gpms.categorical :as categorical]))
+
+(def categorical-pgpm
+  (let [var-name "categorical"
+        parameters {:p {"a" 0.4 "b" 0.5 "c" 0.1}}
+        suff-stats {:n 0 :counts {"a" 0 "b" 0 "c" 0}}]
+    (categorical/spec->categorical var-name parameters suff-stats)))
+
+(deftest logpdf
+  (let [targets {"categorical" "a"}
+        constraints {"categorical" "b"}]
+    (is (= (double (/ 1 3)) (Math/exp (gpm.proto/logpdf categorical-pgpm targets {}))))
+    (is (= 1.0 (Math/exp (gpm.proto/logpdf categorical-pgpm {} {}))))
+    (is (= 1.0 (Math/exp (gpm.proto/logpdf categorical-pgpm targets targets))))
+    (is (= ##-Inf (gpm.proto/logpdf categorical-pgpm targets constraints)))))
+
+(deftest simulate
+  (let [n 100000
+        error-margin 0.01
+        targets []
+        constraints {}
+        samples (frequencies (gpm.proto/simulate categorical-pgpm targets constraints n))]
+    (mapv (fn [k]
+           (is (< (utils/abs (- (/ (get samples k) n)
+                                (/ 1 3)))
+                  error-margin)))
+          (-> categorical-pgpm
+              (get-in [:parameters :p])
+              (keys)))))
+
+(defn check-suff-stats
+  [stattype suff-stats]
+  (is (= (:suff-stats stattype) suff-stats))
+  stattype)
+
+(def data
+  {0 "a"
+   1 "b"
+   2 "c"})
+
+(deftest incorporate-unincorporate
+  (let [suff-stats-0 {:n 0 :counts {"a" 0 "b" 0 "c" 0}}
+        suff-stats-1 {:n 1 :counts {"a" 1 "b" 0 "c" 0}}
+        suff-stats-2 {:n 2 :counts {"a" 1 "b" 1 "c" 0}}
+        suff-stats-3 {:n 1 :counts {"a" 0 "b" 1 "c" 0}}]
+    (-> categorical-pgpm
+        (check-suff-stats suff-stats-0)
+        (gpm.proto/incorporate {(:var-name categorical-pgpm) (get data 0)})
+        (check-suff-stats suff-stats-1)
+        (gpm.proto/incorporate {(:var-name categorical-pgpm) (get data 1)})
+        (check-suff-stats suff-stats-2)
+        (gpm.proto/unincorporate {(:var-name categorical-pgpm) (get data 0)})
+        (check-suff-stats suff-stats-3))))
+
+(deftest logpdf-score
+  (let [one-observation (gpm.proto/incorporate categorical-pgpm {(:var-name categorical-pgpm) (get data 0)})
+        two-observations (-> categorical-pgpm
+                             (gpm.proto/incorporate {(:var-name categorical-pgpm) (get data 0)})
+                             (gpm.proto/incorporate {(:var-name categorical-pgpm) (get data 1)}))
+        score-one (gpm.proto/logpdf-score one-observation)
+        score-two (gpm.proto/logpdf-score two-observations)]
+    (is (> score-one score-two))))
+
+(deftest categorical?
+  (is (categorical/categorical? categorical-pgpm))
+  (is (not (categorical/categorical? {:p {"a" (/ 1 3) "b" (/ 1 3) "c" (/ 1 3)}}))))
+
+(deftest spec->categorical
+  (let [var-name "categorical"
+        parameters {:p {"a" (/ 1 3) "b" (/ 1 3) "c" (/ 1 3)}}
+        categorical (categorical/spec->categorical var-name parameters)]
+    (is (categorical/categorical? categorical))
+    (is (categorical/categorical? (categorical/spec->categorical var-name parameters {:n 10 :counts {"a" 3 "b" 4 "c" 3}})))))
